2da70edd871d68f8f19a7a296acbbbd456c867ff
FlorencioRua <florencio.rua.27@unsch.edu.pe>
2017-07-17 10:24:39 -0500

    Version 1

---
+++ MundoTechMatematica/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MundoTechMatematica.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Models.ComercioEntities bd = new Models.ComercioEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Buscar(string id="")
+        {
+            //acceso a base de datos
+            var cursos = bd.Curso.Where(x => x.Descripcion.Contains(id)).ToList();
+            //lista de productos
+            return View(cursos);
+        }
+
+        public ActionResult Cursodetalle()
+        {
+            return View();
+        }
+
+        public ActionResult Nosotros()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Carrito()
+        {
+            return View();
+        }
+    }
+}
